38604adc05659d2820bfd25391bdc6b093498302
Shreyas <shreyassalyan917@gmail.com>
2024-10-04 10:05:43 +0530

    fixed some issues

---
+++ types/lucide-react.d.ts
@@ -0,0 +1,5 @@
+// declare module 'lucide-react' {
+//     // Declare any specific components or exports you want to use
+//     export const Lucide: any; // Replace with specific exports as needed
+//     export default Lucide;
+// }
